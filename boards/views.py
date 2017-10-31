013e3963bab29a7f2953fcf2640798e9a00a170d
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-10-31 18:07:38 -0200

    list topics from a specific board

--- boards/views.py
+++ boards/views.py
@@ -5,3 +5,8 @@ def home(request):
     boards = Board.objects.all()
     context = {'boards': boards}
     return render(request, 'home.html', context)
+
+
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board': board})
