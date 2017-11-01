b02cc3acd994e1d55a64cc8c51bb6da92fb9d45f
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-10-31 23:26:25 -0200

    topics list tested and working

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Board
 
 def home(request):
@@ -8,5 +9,5 @@ def home(request):
 
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
+    board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
