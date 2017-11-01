e543655ff2c2cadf3e14f44b5232966480b5db83
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-11-01 18:51:04 -0200

    settings refactored

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,15 +30,24 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boards'
-]
+)
+
+LOCAL_APPS = (
+    'boards',
+)
+
+THIRD_PARTY_APPS = (
+    'widget_tweaks',
+)
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
