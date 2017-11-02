9b35c1d6df1680c9a17150d56d43f70feb46ce22
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-11-01 23:56:10 -0200

    signup view sketch added

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
-from boards import views
+
+from accounts import views as accounts_views
+from boards import views as boards_views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
-    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^$', boards_views.home, name='home'),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^boards/(?P<pk>\d+)/$', boards_views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', boards_views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
 ]
