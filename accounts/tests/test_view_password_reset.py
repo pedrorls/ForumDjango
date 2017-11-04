70caab870cf3395d81035b278dd7b647425aae62
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-11-04 21:52:22 -0200

    confirm reset tests created

--- accounts/tests/test_view_password_reset.py
+++ accounts/tests/test_view_password_reset.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth import views as auth_views
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.core.urlresolvers import reverse
 from django.core import mail
 from django.urls import resolve
@@ -70,3 +73,33 @@ class DonePasswordResetTests(TestCase):
     def test_done_reset_view_function(self):
         view = resolve('/reset/done/')
         self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
+
+
+class ConfirmPasswordResetTests(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='pedro', password='senha123', email='pedro@example.com')
+        self.uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
+        self.token = default_token_generator.make_token(user)
+        url = reverse('password_reset_confirm', kwargs={'uidb64': self.uid, 'token': self.token})
+        self.response = self.client.get(url, follow=True)
+
+    def test_confirm_reset_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_confirm_reset_view_function(self):
+        view = resolve('/reset/{uidb64}/{token}/'.format(
+            uidb64=self.uid,
+            token=self.token
+            ))
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetConfirmView)
+    
+    def test_confirm_reset_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_confirm_reset_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, SetPasswordForm)
+
+    def test_confirm_reset_form_inputs(self):
+        self.assertContains(self.response, '<input', 3)
+        self.assertContains(self.response, 'type="password', 2)
