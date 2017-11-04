929bc267c3af294881839e8e40fe207df29b6cb1
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-11-04 20:10:36 -0200

    temmplate tags function tested

--- boards/templatetags/form_tags.py
+++ boards/templatetags/form_tags.py
@@ -11,7 +11,7 @@ def input_class(bound_field):
     css_class = ''
     if bound_field.form.is_bound:
         if bound_field.errors:
-            css_class = 'is_invalid'
+            css_class = 'is-invalid'
         elif field_type(bound_field) != 'PasswordInput':
             css_class = 'is-valid'
     return 'form-control {}'.format(css_class)
