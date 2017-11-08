5aef9bb9a9dd2e6d4e23d59a4a0f28720ec941e0
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-11-08 14:54:20 -0200

    form for reply post added

--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
-from .forms import NewTopicForm
+from .forms import *
 from .models import *
 
 def home(request):
@@ -32,7 +32,22 @@ def new_topic(request, pk):
                 topic = topic,
                 created_by = request.user
             )
-            return redirect('board_topics', pk=board.pk)
+            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+    else:
+        form = PostForm()
+    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
