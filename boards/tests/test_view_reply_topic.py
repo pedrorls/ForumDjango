9358c6a8f3b70926350c923c3585046d5236ebcf
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-11-08 23:21:23 -0200

    reply view tests refactored

--- boards/tests/test_view_reply_topic.py
+++ boards/tests/test_view_reply_topic.py
@@ -18,8 +18,6 @@ class ReplyTopicsTestCase(TestCase):
 
 
 class LoginRequiredReplyTopicTests(ReplyTopicsTestCase):
-    def login(self):
-        self.client.login(username='Pedro', password='senha123')
 
     def test_redirection(self):
         login_url = reverse('login')
@@ -27,55 +25,50 @@ class LoginRequiredReplyTopicTests(ReplyTopicsTestCase):
         self.assertRedirects(response, '{0}?next={1}'.format(login_url, self.url))
 
     def test_user_is_autheticated(self):
-        self.login()
+        self.client.login(username='Pedro', password='senha123')
         response = self.client.get(reverse('home'))
         user = response.context.get('user')
         self.assertTrue(user.is_authenticated)
 
 
 class ReplyTopicTests(ReplyTopicsTestCase):
-    def login(self):
+    def setUp(self):
+        super().setUp()
         self.client.login(username='Pedro', password='senha123')
 
     def test_reply_topic_status_code(self):        
-        self.login()
         response = self.client.get(self.url)
         self.assertEquals(response.status_code, 200)
 
     def test_reply_invalid_topic_status_code(self):
-        self.login()
         url = reverse('reply_topic', kwargs={'pk': self.board.pk, 'topic_pk': 99})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 404)
 
     def test_reply_view_resolves_reply_topic_view(self):
-        self.login()
         view = resolve('/boards/1/topics/1/reply/')
         self.assertEquals(view.func, reply_topic)
 
     def test_reply_view_shows_form(self):
-        self.login()
         response = self.client.get(self.url)
         form = response.context.get('form')
         self.assertIsInstance(form, PostForm)
 
     def test_reply_view_form_csrf(self):
-        self.login()
         response = self.client.get(self.url)
         self.assertContains(response, 'csrfmiddlewaretoken')
 
     def test_reply_view_form_inputs(self):
-        self.login()
         response = self.client.get(self.url)
         self.assertContains(response, '<input', 1)
 
 
 class SuccessfulReplyTopicTests(ReplyTopicsTestCase):
-    def login(self):
+    def setUp(self):
+        super().setUp()
         self.client.login(username='Pedro', password='senha123')
 
     def test_new_reply_valid_post_data(self):
-        self.login()
         data = {
             'message': 'Nothing relevant to say here'
         }
@@ -83,7 +76,6 @@ class SuccessfulReplyTopicTests(ReplyTopicsTestCase):
         self.assertTrue(Topic.objects.exists())
 
     def test_reply_redirect_after_valid_reply(self):
-        self.login()
         data = {
             'message': 'Nothing relevant to say here'
         }
@@ -93,17 +85,17 @@ class SuccessfulReplyTopicTests(ReplyTopicsTestCase):
 
 
 class InvalidReplyTopicTests(ReplyTopicsTestCase):
-    def login(self):
+    def setUp(self):
+        super().setUp()
         self.client.login(username='Pedro', password='senha123')
 
+
     def test_new_reply_invalid_empty_post_data(self):
-        self.login()
         response = self.client.post(self.url, {})
         form = response.context.get('form')
         self.assertTrue(form.errors)
 
     def test_invalid_new_reply_does_not_create_object(self):
-        self.login()
         old = Topic.objects.count()
         self.client.post(self.url, {})
         new = Topic.objects.count()  
